82cb17b256a1f35fff72c315498a9f5f50b6cfcc
eculob <87715619+eculob@users.noreply.github.com>
2021-07-21 02:34:15 +0800

    Add files via upload

---
+++ database/migrations/2020_11_25_100439_create_settings_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+         Schema::create('settings', function (Blueprint $table) {
+            $table->id();
+            $table->string('app_name');
+            $table->string('email');
+            $table->string('currency')->default('$');
+            $table->string('currency_code')->default('USD');
+            $table->string('service_fee');
+            $table->integer('per_page_data')->default(6);
+            $table->string('frontsite_link')->nullable();
+            $table->string('cookie_link')->nullable();
+            $table->string('privacy_link')->nullable();
+            $table->string('term_link')->nullable();
+            $table->string('help_link')->nullable();
+            $table->string('insta_link')->nullable();
+            $table->string('stripe_mode')->default('SANDBOX');
+            $table->string('stripe_test_secret_key')->nullable();
+            $table->string('stripe_test_pub_key')->nullable();
+            $table->string('stripe_live_secret_key')->nullable();
+            $table->string('stripe_live_pub_key')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('settings');
+    }
+}
